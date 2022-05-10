81a083986cab16528d7c03eadb2e63902f036455
MarcinLitwin <105214740+MarcinLitwin@users.noreply.github.com>
2022-05-10 12:26:07 +0200

    nwm

--- kalkulator/script.js
+++ kalkulator/script.js
@@ -1,15 +1,36 @@
-const userInput = document.getElementById('input')
-const numberAdd = document.getElementById('add')
-const numberSubstract = document.getElementById('subtract')
-const numberMultiply = document.getElementById('multiply')
-const numberDivide = document.getElementById('divide')
-const wynikRownania = document.getElementById('wynik')
+const userInput = document.getElementById('input');
+const numberAdd = document.getElementById('add');
+const numberSubstract = document.getElementById('subtract');
+const numberMultiply = document.getElementById('multiply');
+const numberDivide = document.getElementById('divide');
+const wynikRownania = document.getElementById('rownaSie');
 
-const resultOutput = document.getElementById('result')
+const resultOutput = document.getElementById('result');
 
-const numberReset = document.getElementById('zerowanie')
+const numberReset = document.getElementById('zerowanie');
 
-function currentResultOutput(result){
-    resultOutput.textContent = result
-
+function currentResultOutput(result) {
+  resultOutput.textContent = result;
 }
+
+document.addEventListener('keypress', (event) => {
+  var name = event.key;
+  var code = event.code;
+  // Alert the key name and key code on keydown
+  //alert(`Key pressed ${name} \r\n Key code value: ${code}`);
+  if (name== "+"){
+    add()
+  }
+  else if(name =="-"){
+    subtract()
+  }
+  else if (name == "/"){
+    divide()
+  }
+  else if (name == "*"){
+    multiply()
+  }
+  else{
+    return
+  }
+}, false);
--- kalkulator/script2.js
+++ kalkulator/script2.js
@@ -12,88 +12,83 @@ function add() {
   wynik = podaneLiczby + wynik;
   currentResultOutput(wynik);
   document.getElementById('input').value = '';
-  poprzednieDzialanie = "+"
+  poprzednieDzialanie = '+';
 }
 
 function subtract() {
   const podaneLiczby = getUserInput();
   wynik = wynik - podaneLiczby;
   currentResultOutput(wynik);
-  poprzednieDzialanie = "-"
+  poprzednieDzialanie = '-';
   document.getElementById('input').value = '';
 }
 
 function multiply() {
   const podaneLiczby = getUserInput();
-  if (wynik == 0) {
-    wynik++;
+  let czyZero = wynik;
+
+  if (czyZero == 0) {
+    wynik = podaneLiczby;
+    currentResultOutput(wynik);
+    poprzednieDzialanie = '*';
+    document.getElementById('input').value = '';
+  } else {
+    wynik = podaneLiczby * wynik;
+    currentResultOutput(wynik);
+    poprzednieDzialanie = '*';
+    document.getElementById('input').value = '';
   }
-  wynik = podaneLiczby * wynik;
-  currentResultOutput(wynik);
-  poprzednieDzialanie = "*"
-  document.getElementById('input').value = '';
 }
 
 function divide() {
   const podaneLiczby = getUserInput();
-  wynik = podaneLiczby;
-  currentResultOutput(wynik);
-  poprzednieDzialanie = "/"
-  document.getElementById('input').value = '';
+  let czyZero = wynik;
+  if (podaneLiczby == 0) {
+    alert('nie mozna dzielic przez zero');
+  } else if (czyZero == 0) {
+    wynik = podaneLiczby;
+    currentResultOutput(wynik);
+    poprzednieDzialanie = '/';
+    document.getElementById('input').value = '';
+  } else {
+    wynik = wynik / podaneLiczby;
+    currentResultOutput(wynik);
+    poprzednieDzialanie = '/';
+    document.getElementById('input').value = '';
+  }
 }
 
 function reset() {
   const nowyWynik = 0;
   wynik = nowyWynik;
   currentResultOutput(nowyWynik);
-  poprzednieDzialanie = " "
+  poprzednieDzialanie = ' ';
 }
 
-function naszWynik(){
-    const podaneLiczby = getUserInput();
-    
-    if (poprzednieDzialanie == "+"){
-        wynik = podaneLiczby + wynik;
-        currentResultOutput(wynik);
-    }
-    else if (poprzednieDzialanie== "-"){
-            wynik = wynik - podaneLiczby;
-            currentResultOutput(wynik);
-        }
-        else if (poprzednieDzialanie== "*"){
-                wynik = podaneLiczby * wynik;
-                currentResultOutput(wynik);
-        
-            }
-          else if (poprzednieDzialanie== "/"){
-                if (podaneLiczby == 0) {
-                  alert('nie mozna dzielic przez zero');
-                }
-                wynik = wynik / podaneLiczby;
-                currentResultOutput(wynik);
-            }
-            else{
-                return
-            }
-          
-        
-    
+function naszWynik() {
+  const podaneLiczby = getUserInput();
+  let dzialanie = poprzednieDzialanie;
+  if (dzialanie == '+') {
+    wynik = podaneLiczby + wynik;
+    currentResultOutput(wynik);
+  } else if (dzialanie == '-') {
+    wynik = wynik - podaneLiczby;
+    currentResultOutput(wynik);
+  } else if (dzialanie == '*') {
+    wynik = podaneLiczby * wynik;
+    currentResultOutput(wynik);
+  } else if (dzialanie == '/') {
+    wynik = wynik / podaneLiczby;
+    currentResultOutput(wynik);
+  } else {
+    return;
+  }
 
-   logEntries = " "
-    document.getElementById('input').value = '';
+  logEntries = ' ';
+  document.getElementById('input').value = '';
 }
 
-
-function puste(){
-    const podaneLiczby = getUserInput();
-    if (podaneLiczby==" "){
-        podaneLiczby = wynik
-    }
-}
-
-
-
-wynikRownania.addEventListener('click',naszWynik)
+wynikRownania.addEventListener('click', naszWynik);
 numberAdd.addEventListener('click', add);
 numberSubstract.addEventListener('click', subtract);
 numberMultiply.addEventListener('click', multiply);
